639a917f2b7ed3a39a0ad225b96007441e656580
ry brown <ryan.alex.brown@gmail.com>
2020-08-30 15:03:25 -0700

    all done

--- assets/js.js
+++ assets/js.js
@@ -1,9 +1,11 @@
+
+ 
 $("#currentDay").text(moment().format("MMMM Do YYYY"));
 console.log ("currentDay");
 
 var hour = moment().hours();
 console.log(hour);
-
+localStorage.getItem
 
 $("textarea").each(function(){
     var rowTime = $(this).attr("id");
@@ -19,6 +21,53 @@ $("textarea").each(function(){
     };
 
 });
+
+function setUp() {
+
+    var hour9 = JSON.parse(localStorage.getItem("9AM"));
+    $("#9").val(hour9);
+
+    var hour10 = JSON.parse(localStorage.getItem("10AM"));
+    $("#10").val(hour10);
+
+    var hour11 = JSON.parse(localStorage.getItem("11AM"));
+    $("#11").val(hour11);
+
+    var hour12 = JSON.parse(localStorage.getItem("12PM"));
+    $("#12").val(hour12);
+
+    var hour13 = JSON.parse(localStorage.getItem("1PM"));
+    $("#13").val(hour13);
+
+    var hour14 = JSON.parse(localStorage.getItem("2PM"));
+    $("#14").val(hour14);
+
+    var hour15 = JSON.parse(localStorage.getItem("3PM"));
+    $("#15").val(hour15);
+
+    var hour16 = JSON.parse(localStorage.getItem("4PM"));
+    $("#16").val(hour16);
+
+    var hour17 = JSON.parse(localStorage.getItem("5PM"));
+    $("#17").val(hour17);
+  
+  }; 
+
+
+$(".saveBtn").on("click",function(){
+    var text = $(this).siblings("textarea").val();
+    var hour= $(this).siblings("div").text();
+    
+
+    localStorage.setItem(hour, JSON.stringify(text));
+});
+
+setUp();
+
+
+
+
+
 
 
 
