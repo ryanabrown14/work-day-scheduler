58ddab0a2202e42d210349d8030b7857acc7bffc
ry brown <ryan.alex.brown@gmail.com>
2020-08-29 23:06:29 -0700

    row color changes w/ time of day

--- assets/js.js
+++ assets/js.js
@@ -1,3 +1,25 @@
 $("#currentDay").text(moment().format("MMMM Do YYYY"));
 console.log ("currentDay");
 
+var hour = moment().hours();
+console.log(hour);
+
+
+$("textarea").each(function(){
+    var rowTime = $(this).attr("id");
+    var number = parseInt(rowTime);
+    if (number === hour){
+        $(this).addClass("present");
+    }
+    else if (number > hour){
+        $(this).addClass("future");
+    }
+    else {
+        $(this).addClass("past");
+    };
+
+});
+
+
+
+
